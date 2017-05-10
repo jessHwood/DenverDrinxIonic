b5a2650329920d765894f4bee4b2b47030b047ce
ntutchton <ntutchton@gmail.com>
2017-05-10 15:49:43 -0600

    populating bars from hardcoded data

--- DenverDrinx/src/pages/list/list.ts
+++ DenverDrinx/src/pages/list/list.ts
@@ -10,20 +10,34 @@ import { BarDetailsPage } from '../bar-details/bar-details';
 })
 export class ListPage {
   icons: string[];
-  bars: Array<{title: string, note: string, icon: string}>;
+  bars: Array<{ }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.icons = ['flask', 'beer', 'football', 'basketball',
+    'american-football'];
 
     this.bars = [];
+
+    //this loop pushes 11 fake bars into the list
+    //this is where we will do our pai call to fill the list
     for(let i = 1; i < 11; i++) {
       this.bars.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    day           : [ 4 ],
+    hours         : [ [ 14 , 17 ] ],
+    minutes       : [ [ 0 , 30 ] ],
+    address       : "1234 Seseme Street",
+    name          : "Big Bird's Booze House",
+    drinkSpecials : "$1.00 Double-Shots",
+    foodSpecials  : "$0.50 Corn-dogs",
+    //we will need to replace "icon" with an image in the server and here
+    icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+
+    /////--working on making api calls to fill bar list below--//////
+    // this.http.get('https://randomuser.me/api/?results=1')
+    //   .then(function(response) { console.log(response); })
+    //////////////////////--end api work--///////////////////////////
   }
 
   barTapped(event, bar) {
