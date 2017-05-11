eed07da563fada57cca2497f958e4a5e994a5c4f
CamB17 <camcodes@Camerons-MacBook-Pro.local>
2017-05-11 17:26:13 -0600

    Creates styling to tabs, and bar page

--- DenverDrinx/www/js/factories.js
+++ DenverDrinx/www/js/factories.js
@@ -15,15 +15,22 @@ angular.module('factories', [])
     id : 1
   },
   {
-    day       : [ 4 ],
-    hours       : [ [ 14 , 17 ] ],
-    minutes     : [ [ 0 , 30 ] ],
-    address     : "5678 Seseme Street",
-    name      : "Oscar's Hootch Can",
-    drinkSpecials : "$5 PBRs",
-    foodSpecials  : "Used Pringles",
-    id : 2
-  }];
+        day             : [ 1, 2, 3, 4, 5 ],
+        hours             : [ [15 , 22], [15 , 18], [15 , 18], [15 , 18], [15 , 18] ],
+        minutes         : [ [ 0 , 0 ], [ 0 , 0 ], [ 0 , 0 ], [ 0 , 0 ] ],
+        address            : "The Source, 3350 Brighton Boulevard, Unit 105",
+        phone            : "(303) 296-2747",
+        name            : "Comida",
+        drinkSpecials    : "$5 Margaritas and Cocktails, $2-$3 Beers, and $5 House Wines.",
+        foodSpecials    : "$4 Street Snacks, $15 Bacon Tacos with a shot of Maker's Mark",
+        image            : "https://cdn.wedding-spot.com/images/venues/2751/comida-at-the-source-wedding-denver-co-1.1427695390.jpg",
+        description        : "Sit-down extension of a popular food truck featuring Mexican street eats & creative margaritas.",
+        website            : "eatcomida.com/en/",
+        id: 2
+    },
+
+
+  ];
 
   return {
     all: function() {
