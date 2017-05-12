978cef97575911ac33be59a7a31c85a28045cf22
ntutchton <ntutchton@gmail.com>
2017-05-12 12:32:15 -0600

    test

--- DenverDrinx/www/js/controllers.js
+++ DenverDrinx/www/js/controllers.js
@@ -5,7 +5,7 @@ angular.module('controllers', [])
 .controller('BarDetailCtrl', BarDetailCtrl)
 .controller('UberCtrl', UberCtrl)
 
-.controller('AccountCtrl', AccountCtrl);
+.controller('AccountCtrl', AccountCtrl); //to be scapped
 
 MapCtrl.$inject = ['$cordovaGeolocation', 'Bars', '$http'];
 function MapCtrl($cordovaGeolocation, Bars, $http) {
@@ -28,9 +28,9 @@ function MapCtrl($cordovaGeolocation, Bars, $http) {
     //this marker is the user's location
       var marker = new google.maps.Marker({
         map: self.map,
-        animation: google.maps.Animation.DROP,
         position: latLng,
-        icon: "../img/markTEST.png"
+        icon: "../img/meMarker.png"
+        
       });  
 
       //for each bar, http request google and drop a marker on the map
@@ -43,8 +43,8 @@ function MapCtrl($cordovaGeolocation, Bars, $http) {
             var marker = new google.maps.Marker({
                 map: self.map,
                 animation: google.maps.Animation.DROP,
-                position: location.data.results[0].geometry.location
-                //icon: [icon location]
+                position: location.data.results[0].geometry.location,
+                icon: '../img/drinkTEST.png'
             });  
           });   
       });     
@@ -52,47 +52,35 @@ function MapCtrl($cordovaGeolocation, Bars, $http) {
   }, function(error){
     console.log("Could not get location");
   });
+}//end MapCtrl
 
-
-
-}
-
-function BarsCtrl(Bars) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+BarsCtrl.$inject = ['Bars', '$http', '$cordovaGeolocation'];
+function BarsCtrl(Bars, $http, $cordovaGeolocation) {
   var self = this;
   self.bars = Bars.all();
 
 //create timer of each bar
   self.bars.forEach(function(bar){
     bar.timeLeft = function(){
-      //console.log(this);
-      //console.log('running time left' + bar.name);
       var currentTime = new Date();
       var timer = 0;
 
       for (i = 0; i < this.day.length; i++){
-
+        //if happy hour is today
         if (this.day[i] === currentTime.getDay()) {
-//console.log('here');
+          //get current time
           var currentHour = currentTime.getHours();
           var currentMinutes = currentTime.getMinutes();
-          //console.log('found a happy hour for today!');
-
+         
+          //happy hour has not started yet
           if (currentHour <= this.hours[i][0]){
-            //happy hour has not started yet
 
-           timer += (this.hours[i][0] - currentHour) * 60;
-           timer += (this.minutes[i][0] - currentMinutes);
+            timer += (this.hours[i][0] - currentHour) * 60;
+            timer += (this.minutes[i][0] - currentMinutes);
             if (timer > 0){
               return this.name + ' starts in :' + timer + ' minutes';
             } 
-            //or is in progress
+            //or is happy hour is in progress?
             if (timer <= 0){
               timer = 0;
               timer += (this.hours[i][1] - currentHour) * 60;
@@ -100,16 +88,16 @@ function BarsCtrl(Bars) {
               return this.name + ' has minutes left: ' + timer;
             }
           }
-
+          //happy hour is in progress
           if (currentHour >= this.hours[i][0]){
-            //happy hour is in progress
-              timer = 0;
-              timer += (this.hours[i][1] - currentHour) * 60;
-              timer += (this.minutes[i][1] - currentMinutes);
+            timer = 0;
+            timer += (this.hours[i][1] - currentHour) * 60;
+            timer += (this.minutes[i][1] - currentMinutes);
+            //still time left!
             if (timer > 0){
               return this.name + ' has minutes left: ' + timer;
             }   
-            //or is over (check minutes & end hour)
+            //or has it ended?
             if (timer <= 0) {
               return this.name + ' is over';
             }
@@ -117,8 +105,7 @@ function BarsCtrl(Bars) {
         }
       }
     };
-  });
-  //end timer
+  }); //end timer
 
 }
 
